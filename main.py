3dc66eb6dc6f6e585def01019bb7c3b998474006
Rishi Banerjee <rishieric91@gmail.com>
2023-08-30 12:37:11 +0530

    Added wake and other response cycle code
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from multiprocessing import process
 import os
 import random
 from email.mime import audio
@@ -73,12 +74,73 @@ conversation = []
 recognizer = sr.Recognizer()
 
 
-async def update_conversation(input, output):
+def update_conversation(input, output):
     conversation.append(
         {'role': 'user', 'content': input})
     conversation.append(
         {'role': 'assistant', 'content': output})
     print("Conversation Updated")
+
+
+def process_instruction(source):
+    while True:
+        audio = recognizer.listen(source)
+
+        recognized_text = recognizer.recognize_google(audio)
+
+        print(f"Recognized Instruction : {recognized_text}")
+
+        doc = nlp(recognized_text)
+
+        play_intent = any((token.text.lower() == "play" and (token.text.lower(
+        ) == 'song' or token.text.lower() == 'rhyme')) or token.pos_ == "VERB" for token in doc)
+        stop_intent = any(
+            (token.text.lower() == "stop" or token.pos_ == "VERB") or
+            (token.text.lower() == "quit" or token.pos_ == "VERB") or
+            (token.text.lower() == "exit" or token.pos_ == "VERB") or
+            (token.text.lower() == "end" or token.pos_ == "VERB")
+            for token in doc
+        )
+
+        print(f"Play Intent? {play_intent}")
+        print(f"Stop Intent? {stop_intent}")
+
+        if play_intent:
+            similar_song_found, song_path = check_similar_song(
+                recognized_text)
+            print(
+                f"Similar Song Found ? {similar_song_found} and Path {song_path}")
+
+            if song_path is not None:
+                play_audio(song_path)
+                update_conversation(
+                    recognized_text, f"Played song {song_path}")
+            else:
+                random_song = random.choice(audio_files)
+                update_conversation(random_song, f"Played song {random_song}")
+                play_audio(random_song)
+        elif stop_intent:
+            output_voice("Sure thing! Stopping")
+            return
+        else:
+            data = {
+                'input': recognized_text, 'age': 4,
+            }
+
+            if len(conversation) > 0:
+                data['conversation'] = json.dumps(conversation)
+
+            response = r.post(API_URL, json=data)
+
+            print(response.status_code)
+
+            speech = response.json()['response']
+            update_conversation(recognized_text, speech)
+
+            print(f"Response : {conversation}")
+
+            output_voice(speech)
+
 
 def speech_to_text():
 
@@ -91,64 +153,22 @@ def speech_to_text():
             audio = recognizer.listen(source)
 
             print("There was some audio input!")
-            
+
             try:
                 recognized_text = recognizer.recognize_google(audio)
                 print(f"Recognized Text : {recognized_text}")
-                
-                wake_word = fuzz.partial_ratio(recognized_text, "hey siri")
+
+                wake_word = fuzz.partial_ratio(recognized_text, "hey google")
 
                 print(f"Wake Word Spoken: {wake_word}")
 
                 if wake_word > 70:
-                    output_voice(random.choice(['yes', 'yes tell me', 'sup', 'whats up', 'yo yo']))
+                    output_voice(random.choice(
+                        ['yes', 'yes tell me', 'sup', 'whats up', 'yo yo']))
 
-                    audio = recognizer.listen(source)
-
-                    recognized_text = recognizer.recognize_google(audio)
-
-                    print(f"Recognized Instruction : {recognized_text}")
-
-                    doc = nlp(recognized_text)
-
-                    play_intent = any((token.text.lower() == "play" and (token.text.lower(
-                    ) == 'song' or token.text.lower() == 'rhyme')) or token.pos_ == "VERB" for token in doc)
-
-                    print(f"Play Intent? {play_intent}")
-
-                    if play_intent:
-                        similar_song_found, song_path = check_similar_song(
-                            recognized_text)
-                        print(
-                            f"Similar Song Found ? {similar_song_found} and Path {song_path}")
-
-                        if song_path is not None:
-                            play_audio(song_path)
-                            update_conversation(recognized_text, f"Played song {song_path}")
-                        else:
-                            random_song = random.choice(audio_files)
-                            update_conversation(random_song, f"Played song {random_song}")
-                            play_audio(random_song)
-                    else:
-                        data = {
-                            'input': recognized_text, 'age': 4,
-                        }
-
-                        if len(conversation) > 0:
-                            data['conversation'] = json.dumps(conversation)
-
-                        response = r.post(API_URL, json=data)
-
-                        print(response.status_code)
-
-                        speech = response.json()['response']
-                        update_conversation(recognized_text, speech)
-                        
-
-                        print(f"Response : {conversation}")
-
-                        output_voice(speech)
-                print(f"Keep Sleeping")
+                    process_instruction(source)
+                else:
+                    print(f"Keep Sleeping")
             except Exception as e:
                 print(f"Some Error Occoured : {e}")
 
