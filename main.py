541b6a0dd7a123c0747fa3300d2d90886c6d2918
Rishi Banerjee <rishieric91@gmail.com>
2023-09-01 16:13:20 +0530

    fixed song
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- main.py
+++ main.py
@@ -166,7 +166,63 @@ def speech_to_text():
                     output_voice(random.choice(
                         ['yes', 'yes tell me', 'sup', 'whats up', 'yo yo']))
 
-                    process_instruction(source)
+                    while True:
+                        audio = recognizer.listen(source)
+
+                        recognized_text = recognizer.recognize_google(audio)
+
+                        print(f"Recognized Instruction : {recognized_text}")
+
+                        doc = nlp(recognized_text)
+
+                        play_intent = any((token.text.lower() == "play" or (token.text.lower(
+                        ) == 'song' or token.text.lower() == 'rhyme')) or token.pos_ == "VERB" for token in doc)
+                        stop_intent = any(
+                            (token.text.lower() == "stop" or token.pos_ == "VERB") or
+                            (token.text.lower() == "quit" or token.pos_ == "VERB") or
+                            (token.text.lower() == "exit" or token.pos_ == "VERB") or
+                            (token.text.lower() == "end" or token.pos_ == "VERB")
+                            for token in doc
+                        )
+
+                        print(f"Play Intent? {play_intent}")
+                        print(f"Stop Intent? {stop_intent}")
+
+                        if play_intent:
+                            similar_song_found, song_path = check_similar_song(
+                                recognized_text)
+                            print(
+                                f"Similar Song Found ? {similar_song_found} and Path {song_path}")
+
+                            if song_path is not None:
+                                play_audio(song_path)
+                                update_conversation(
+                                    recognized_text, f"Played song {song_path}")
+                            else:
+                                random_song = random.choice(audio_files)
+                                update_conversation(random_song, f"Played song {random_song}")
+                                play_audio(random_song)
+                        elif stop_intent:
+                            output_voice("Sure thing! Stopping")
+                            return
+                        else:
+                            data = {
+                                'input': recognized_text, 'age': 4,
+                            }
+
+                            if len(conversation) > 0:
+                                data['conversation'] = json.dumps(conversation)
+
+                            response = r.post(API_URL, json=data)
+
+                            print(response.status_code)
+
+                            speech = response.json()['response']
+                            update_conversation(recognized_text, speech)
+
+                            print(f"Response : {conversation}")
+
+                            output_voice(speech)
                 else:
                     print(f"Keep Sleeping")
             except Exception as e:
