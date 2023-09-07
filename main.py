9e5953a4a5afba76e37474a268918fb08a064ca2
Rishi Banerjee <rishieric91@gmail.com>
2023-09-07 21:55:28 +0530

    Optimized certain functios
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import argparse
 import json
-import sys
-from multiprocessing import process
+
+import time
 import os
 import random
-from email.mime import audio
+
 from io import BytesIO
 from tempfile import TemporaryFile
 import asyncio
 import requests as r
-import spacy
 import speech_recognition as sr
 from dotenv import load_dotenv
 from fuzzywuzzy import fuzz
@@ -17,7 +16,22 @@ from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
 from spacy.lang.en import English
-import alsaaudio
+
+from functools import wraps
+
+def timing(f):
+
+    def timed(*args, **kw):
+
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+
+        print('func:%r args:[%r, %r] took: %2.8f sec' % \
+          (f.__name__, args, kw, te-ts))
+        return result
+
+    return timed
 
 
 load_dotenv()
@@ -26,19 +40,19 @@ API_URL = os.getenv('URL')
 
 WAKE_WORD = os.getenv('WAKE_WORD').lower()
 
+play_keywords = {'play', 'song', 'rhyme'}
+stop_keywords = {'stop', 'quit', 'exit', 'end'}
 
 # nlp = spacy.load("en_core_web_sm")
 
 nlp = English()
 
-def mute_microphone():
-    os.system('amixer set Capture nocap')
+trie = None
 
-# Function to unmute the microphone
-def unmute_microphone():
-    os.system('amixer set Capture cap')
 
+@timing # taking 0.0019 sec
 def check_similar_song(user_input: str):
+    print("Using V1 Search Algorithm")
     best_match_ratio = 0
     best_match_path = None
 
@@ -54,7 +68,19 @@ def check_similar_song(user_input: str):
     else:
         return False, None
 
+@timing
+async def output_voicev2(text: str, expect_return=False):
+    tts = gTTS(text, lang='en')
+    fp = BytesIO()
 
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    song = AudioSegment.from_file(fp, format="mp3")
+    play(song)
+
+    return expect_return
+
+@timing
 async def output_voice(text: str, expect_return=False):
     
     tts = gTTS(text, lang='en')
@@ -76,10 +102,6 @@ def play_audio(audio_path: str):
     print(f"Playing: {audio_path}")
 
 
-def test_connection():
-    pass
-
-
 mp3_fp = BytesIO()
 
 
@@ -91,26 +113,22 @@ recognizer = sr.Recognizer()
 microphone = sr.Microphone()
 
 
+@timing
 def update_conversation(input, output):
-    conversation.append(
-        {'role': 'user', 'content': input})
-    conversation.append(
-        {'role': 'assistant', 'content': output})
+    # Single Operation Conversation
+    conversation.extend([
+        {'role': 'user', 'content': input},
+        {'role': 'assistant', 'content': output}
+    ])
     print("Conversation Updated")
 
 async def process_input(recognized_text):
     doc = nlp(recognized_text)
 
-    play_intent = any((token.text.lower() == "play" or (token.text.lower(
-    ) == 'song' or token.text.lower() == 'rhyme')) or token.pos_ == "VERB" for token in doc)
-    stop_intent = any(
-        (token.text.lower() == "stop" or token.pos_ == "VERB") or
-        (token.text.lower() == "quit" or token.pos_ == "VERB") or
-        (token.text.lower() == "exit" or token.pos_ == "VERB") or
-        (token.text.lower() == "end" or token.pos_ == "VERB")
-        for token in doc
-    )
-
+    # 0.00012 seconds in intent detection
+    play_intent = any(token.text.lower() in play_keywords or token.pos_ == "VERB" for token in doc)
+    stop_intent = any(token.text.lower() in stop_keywords or token.pos_ == "VERB" for token in doc)
+    
     print(f"Play Intent? {play_intent}")
     print(f"Stop Intent? {stop_intent}")
 
@@ -178,28 +196,22 @@ async def speech_to_text():
                 if wake_word > 70:
                     if recognized_text.lower().startswith(WAKE_WORD) and len(recognized_text) == len(WAKE_WORD):
                         while True:
-                            # recognizer.pause_threshold()
-                            # mute_microphone()
-                            response = await output_voice(voice_filler(), expect_return=True)
+                            # TODO: Trigger LED LIGHT
+                            # response = await output_voice(voice_filler(), expect_return=True)
+
+                            print("ARE WE READY TO LISTEN")
+                            print("Microphone is back Online")
                             
-                            if response:
+                            print("Listeing for second command!")
+                            audio = recognizer.listen(source)
 
-                                print("ARE WE READY TO LISTEN")
-                                # unmute_microphone()
-                                print("Microphone is back Online")
-                                # break
-                                # audio captured again after normal wake word is detected
-                                # with sr.Microphone as source2:
-                                print("Listeing for second command!")
-                                audio = recognizer.listen(source)
+                            print("I GUESS WE ARE")
 
-                                print("I GUESS WE ARE")
+                            recognized_text = recognizer.recognize_google(audio)
 
-                                recognized_text = recognizer.recognize_google(audio)
+                            print(f"Recognized Instruction : {recognized_text}")
 
-                                print(f"Recognized Instruction : {recognized_text}")
-
-                                await process_input(recognized_text)
+                            await process_input(recognized_text)
                         
                     else :
                         command = recognized_text.lower().replace(WAKE_WORD, "").strip()
@@ -223,6 +235,9 @@ def load_audio_files():
     for filename in os.listdir(folder_path):
         if filename.lower().endswith('.mp3'):
             audio_files.append(os.path.join(folder_path, filename))
+    
+    global trie  # Assuming trie is a global variable
+    trie = build_trie(map(str.lower, map(os.path.basename, audio_files)))
 
 
 async def main():
