8586d9f47aae65cfcc4934e989d79e2f20b480ba
Rishi Banerjee <rishieric91@gmail.com>
2023-09-17 19:24:11 +0530

    boot script created
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- boot_script.sh
+++ boot_script.sh
@@ -15,11 +15,57 @@ check_access_point() {
     fi
 }
 
-if check_internet; then
-    # Code block to run when there is internet
-    echo "Internet is available. Running in Normal Mode"
+check_network_blocks() {
+    local wpa_file="/etc/wpa_supplicant/wpa_supplicant.conf"
     
+    if grep -q "network={" "$wpa_file"; then
+        return 0  # Return true
+    else
+        return 1  # Return false
+    fi
+}
+
+if check_network_blocks; then
+    echo "Network Block is Available"
+    if check_internet; then
+        # Code block to run when there is internet
+        echo "Internet is available. Running in Normal Mode"
+
+        # start falcon mini service
+    else
+        echo "Turning Off Access Point Mode"
+        # Stop Services
+        sudo systemctl stop hostapd
+        sudo systemctl stop dnsmasq
+
+        # Remove dhcpcd configuration
+        sudo sed -i '/interface wlan0/,+2d' /etc/dhcpcd.conf
+
+        # Restore original dnsmasq configuration
+        sudo mv /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
+
+        # Remove hostapd configuration
+        sudo rm /etc/hostapd/hostapd.conf
+
+        # Restore original hostapd default file
+        sudo sed -i 's/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/#DAEMON_CONF=""/' /etc/default/hostapd
+
+        # Disable IP Forwarding
+        sudo sed -i '/^net.ipv4.ip_forward=1/s/^/#/' /etc/sysctl.conf
+        sudo sysctl -p
+
+        # Remove NAT rules
+        sudo iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+        sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+        # Remove iptables restore command from rc.local
+        sudo sed -i '/iptables-restore < \/etc\/iptables.ipv4.nat/d' /etc/rc.local
+
+        # Reboot
+        sudo reboot
     # Start Falcon Mini Service
+else if check_access_point; then
+    echo "Device running in Access Point Mode, User Needs to Share WiFi Credentials"
 else
     # Code block to run when there is no internet
     echo "Internet is not available. Turning on Access Point"
