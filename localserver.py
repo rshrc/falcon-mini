0f5063b2bab5f3db947935dbfbb1d00a03aa6bbe
Rishi Banerjee <rishieric91@gmail.com>
2023-09-17 14:33:26 +0530

    Modifications
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- localserver.py
+++ localserver.py
@@ -7,10 +7,10 @@ def set_wifi_credentials():
     try:
         wifi_ssid = request.json.get('ssid')
         wifi_password = request.json.get('password')
-      #  with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as file:
-       #     file.write(f'\n\nnetwork={{\n    ssid="{wifi_ssid}"\n    psk="{wifi_password}"\n    key_mgmt=WPA-PSK\n}}\n')
-        os.system("sudo echo Hello >> /etc/wpa_supplicant/wpa_supplicant.conf")
-	print("J3 Server Online")
+        # with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as file:
+        #     file.write(f'\\n\\nnetwork={{\\n    ssid="{wifi_ssid}"\\n    psk="{wifi_password}"\\n    key_mgmt=WPA-PSK\\n}}\\n')
+        os.system('sudo echo Hello >> /etc/wpa_supplicant/wpa_supplicant.conf')
+        print("J3 Server Online")
         return 'Credentials updated successfully', 200
 
     except Exception as e:
--- setup_localserver.sh
+++ setup_localserver.sh
@@ -7,27 +7,27 @@ APP_DIR=$(pwd)
 mkdir -p $APP_DIR
 
 # Write the Flask app to a file
-cat <<EOL > $APP_DIR/localserver.py
-from flask import Flask, request
+# cat <<EOL > $APP_DIR/localserver.py
+# from flask import Flask, request
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/api/set-wifi-credentials', methods=['POST'])
-def set_wifi_credentials():
-    try:
-        wifi_ssid = request.json.get('ssid')
-        wifi_password = request.json.get('password')
-        with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as file:
-            file.write(f'\\n\\nnetwork={{\\n    ssid="{wifi_ssid}"\\n    psk="{wifi_password}"\\n    key_mgmt=WPA-PSK\\n}}\\n')
-        print("J3 Server Online")
-        return 'Credentials updated successfully', 200
+# @app.route('/api/set-wifi-credentials', methods=['POST'])
+# def set_wifi_credentials():
+#     try:
+#         wifi_ssid = request.json.get('ssid')
+#         wifi_password = request.json.get('password')
+#         with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as file:
+#             file.write(f'\\n\\nnetwork={{\\n    ssid="{wifi_ssid}"\\n    psk="{wifi_password}"\\n    key_mgmt=WPA-PSK\\n}}\\n')
+#         print("J3 Server Online")
+#         return 'Credentials updated successfully', 200
 
-    except Exception as e:
-        return str(e), 500
+#     except Exception as e:
+#         return str(e), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-EOL
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+# EOL
 
 # Install Gunicorn
 sudo pip3 install gunicorn
@@ -43,7 +43,7 @@ After=network.target
 [Service]
 User=rishi
 WorkingDirectory=$APP_DIR
-ExecStart=/usr/local/bin/gunicorn -b 0.0.0.0:5000 localserver:app
+ExecStart=sudo /usr/local/bin/gunicorn -b 0.0.0.0:5000 localserver:app
 Restart=on-failure
 Type=simple
 
