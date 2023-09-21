644ee0c3d4199d0659b1ac66d4a77bdde61ccf63
Rishi <rishieric91@gmail.com>
2023-09-21 18:15:21 +0530

    some more progress
    Signed-off-by: Rishi <rishieric91@gmail.com>

--- display2.py
+++ display2.py
@@ -3,14 +3,16 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import ili9341
 
-BORDER = 20
-FONTSIZE = 24
+BORDER = 10
+FONTSIZE = 14
 
 BAUDRATE = 24000000
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = digitalio.DigitalInOut(board.D24)
 spi = board.SPI()
+
+FOREFROUND_FONT_COLOR = (0, 0, 0)
 
 disp = ili9341.ILI9341(
     spi,
@@ -34,23 +36,29 @@ image = Image.new("RGB", (width, height))
 draw = ImageDraw.Draw(image)
 
 # Draw a green filled box as the background
-draw.rectangle((0, 0, width, height), fill=(0, 255, 0))
+draw.rectangle((0, 0, width, height), fill=FOREFROUND_FONT_COLOR)
 disp.image(image)
 
 # Draw a smaller inner purple rectangle
-draw.rectangle(
-    (BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(170, 0, 136)
-)
+# draw.rectangle(
+#     (BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(170, 0, 136)
+# )
 
-font = ImageFont.truetype("./DejaVuSans.ttf", FONTSIZE)
+font = ImageFont.load_default()
 
-text = "Hello World!"
+text = "A quick brown fox jumped over"
 (x, y, font_width, font_height) = font.getbbox(text)
+
 draw.text(
     (width // 2 - font_width // 2, height // 2 - font_height // 2),
     text,
     fill=(255, 255, 0),
 )
+
+print(f"Line 58 : {font.getlength(text)}")
+
+# y += font.getlength(text)[1]
+# print("Line 52 : ${52}")
 
 # Display image.
 disp.image(image)
