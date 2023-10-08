250303a41990dd04b6088b9fb5367fbdd406cb4a
Rishi Banerjee <rishieric91@gmail.com>
2023-10-08 16:37:03 +0530

    Added the required yaml reader
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- utils/writer.py
+++ utils/writer.py
@@ -1,4 +1,4 @@
-from typing import NoReturn
+from typing import NoReturn, Dict, Any
 import yaml
 
 def generate_config(user_id: int, filename: str='data.yaml') -> NoReturn:
@@ -11,5 +11,15 @@ def generate_config(user_id: int, filename: str='data.yaml') -> NoReturn:
     with open(filename, 'w') as file:
         yaml.safe_dump(data, file, default_flow_style=False)
 
+def read_config() -> Dict[str, Any]:
+    file: str = 'data.yaml'
+
+    with open(file, 'r') as f:
+        data: Dict[str, Any] = yaml.safe_load(f)
+
+    return data
+
 if __name__=='__main__':    
-    generate_config(123)
+    # generate_config(123)
+
+    print(read_config())
