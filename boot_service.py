4a5efac2a9bcb27ba973e31307ace56804b1fa13
Rishi Banerjee <rishieric91@gmail.com>
2023-10-12 22:45:45 +0530

    hmm
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

--- boot_service.py
+++ boot_service.py
@@ -2,8 +2,10 @@ import subprocess
 import time
 from icecream.icecream import IceCreamDebugger
 import os
-
+from oled.lib import DisplayController
 ic = IceCreamDebugger()
+
+display_controller = DisplayController()
 
 
 def system_in_access_point_mode() -> bool:
@@ -27,6 +29,7 @@ def main():
     success = False
     access_point = system_in_access_point_mode()
     print(f"Is Access Point {access_point}")
+    display_controller.render_text_threaded_v2("Currently device is in hotspot mode")
     if  not access_point:
         for _ in range(1):  
             if connect_to_internet():
@@ -35,12 +38,18 @@ def main():
             time.sleep(1)
 
         if success:
+            display_controller.render_text_threaded_v2("Currently device is in hotspot mode")
+
             print("Connected to Internet")
             subprocess.run(['sudo', 'systemctl', 'start', 'falcon_mini.service'])
         else:
+            display_controller.render_text_threaded_v2("Turning on Hotspot")
+
             subprocess.run(["sudo", f"{dir}/setup_access_point.sh"])
             print("Turning into Mock Hotspot")
     else:
+        display_controller.render_text_threaded_v2("Turning Off Hotspot")
+
         subprocess.run(["sudo", f"{dir}/scrap_access_point.sh"])
         print("Access Point Scrapped, Turning on Wifi")
 
