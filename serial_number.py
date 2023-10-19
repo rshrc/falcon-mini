ec6383148aac1e345acf2e8484df6d180fc553d6
Rishi <rishieric91@gmail.com>
2023-10-19 09:35:08 +0530

    serial number figured out
    Signed-off-by: Rishi <rishieric91@gmail.com>

---
+++ serial_number.py
@@ -0,0 +1,12 @@
+def get_serial_number():
+    with open('/proc/cpuinfo', 'r') as f:
+        for line in f:
+            if line.startswith('Serial'):
+                return line.split(':')[1].strip()
+    return None
+
+serial_number = get_serial_number()
+if serial_number:
+    print(f"Serial Number: {serial_number}")
+else:
+    print("Serial Number not found.")
